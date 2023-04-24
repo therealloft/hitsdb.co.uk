e8d6427f46803c1a7986e93228fa68e8a50a0b76
therealloft <stephenpadgham@yahoo.co.uk>
2023-04-24 22:08:03 +0100

    Add project files.

---
+++ www.hitsdb.co.uk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace www.hitsdb.co.uk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
